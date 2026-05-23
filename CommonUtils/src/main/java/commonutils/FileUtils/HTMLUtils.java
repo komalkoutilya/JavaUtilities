3733704486e0eb9d7d43739c07db8182d53450d6
komalkoutilya <komalkoutilya@gmail.com>
2026-05-23 15:05:49 +0530

    gh-5: Completed development for anchor HTML methods

--- CommonUtils/src/main/java/commonutils/FileUtils/HTMLUtils.java
+++ CommonUtils/src/main/java/commonutils/FileUtils/HTMLUtils.java
@@ -6,9 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -35,6 +33,7 @@ public class HTMLUtils {
         }
     }
 
+    private static final String anchorTag = Optional.ofNullable(htmlProperties.getProperty("html.tag.anchor")).orElse("a");
     /**
      * Fetches all HTML tags from html.properties file
      *
@@ -64,7 +63,7 @@ public class HTMLUtils {
         Matcher tagInnerContentMatcher = tagInnerContentPattern.matcher(htmlSnippet);
         List<String> innerHTMLContentValues = new ArrayList<>();
         while (tagInnerContentMatcher.find()) {
-            innerHTMLContentValues.add(tagInnerContentMatcher.group(1));
+            innerHTMLContentValues.add(tagInnerContentMatcher.group(1).strip());
         }
         logger.info("list of tag content values - {}: {}", htmlTag, innerHTMLContentValues);
         return innerHTMLContentValues;
@@ -87,5 +86,69 @@ public class HTMLUtils {
         }
         logger.info("list of attribute values - {}: {}", attribute, attributeValues);
         return attributeValues;
+    }
+
+    /**
+     * Returns the link uri value of the anchor tag matching the given link text.
+     *
+     * @param linkText    the text inside the anchor tag
+     * @param htmlSnippet the HTML snippet containing anchor tags
+     * @return the link value of the matching anchor tag
+     * @throws IllegalArgumentException if the link text is missing or occurs multiple times
+     */
+    public static String getLinkValue(String linkText, String htmlSnippet) {
+        List<String> retrievedLinkTexts = getTagInnerContent(anchorTag, htmlSnippet);
+        if (Collections.frequency(retrievedLinkTexts, Optional.ofNullable(linkText).map(String::strip).orElseThrow(() -> new IllegalArgumentException("Link text should not be null"))) != 1) {
+            throw new IllegalArgumentException("There are multiple links with link text: " + linkText);
+        }
+        return getAttributeValue(anchorTag, "href", htmlSnippet).get(retrievedLinkTexts.indexOf(linkText));
+    }
+
+    /**
+     * Extracts all anchor ({@code <a>}) tag records from the given HTML snippet.
+     * <p>
+     * This method retrieves the link text of all anchor tags and the corresponding {@code href} attribute values and stores them in a {@link Map}, where:
+     * <ul>
+     *     <li>Key   = Link text</li>
+     *     <li>Value = List of corresponding URI values</li>
+     * </ul>
+     * <p>
+     * If multiple anchor tags contain the same link text, all associated href values are grouped into the same list.
+     * <pre>
+     * Example:
+     * {@code
+     * <a href="https://google.com">Google</a> <a href="https://gmail.com">Google</a>
+     * }
+     *
+     * Output:
+     * {
+     *   "Google" -> [
+     *       "https://google.com",
+     *       "https://gmail.com"
+     *   ]
+     * }
+     * </pre>
+     *
+     * @param htmlSnippet the HTML content from which anchor tag information should be extracted
+     * @return a map containing link texts as keys and corresponding href attribute values as lists
+     * @throws IllegalArgumentException if the provided HTML snippet is {@code null} or empty
+     * @throws RuntimeException         if the number of extracted anchor texts and href attribute values do not match
+     */
+    public static Map<String, List<String>> getAllAnchors(String htmlSnippet) throws IllegalArgumentException, RuntimeException {
+        htmlSnippet = Optional.ofNullable(htmlSnippet).map(String::strip).orElse("");
+        if (htmlSnippet.isEmpty()) {
+            throw new IllegalArgumentException("An empty HMTL snippet provided");
+        }
+        List<String> linkTexts = getTagInnerContent(anchorTag, htmlSnippet);
+        logger.debug("Success - Retrieved link texts from html snippet");
+        List<String> linkValues = getAttributeValue(anchorTag, "href", htmlSnippet);
+        logger.debug("Success - Retrived URI values from html snippet");
+        if (linkTexts.size() != linkValues.size())
+            throw new RuntimeException("Error occurred: Link texts and Link values are in different count");
+        Map<String, List<String>> anchorRecords = new HashMap<>();
+        for (int iterator = 0; iterator < linkTexts.size(); iterator++) {
+            anchorRecords.computeIfAbsent(linkTexts.get(iterator), lt -> new ArrayList<>()).add(linkValues.get(iterator));
+        }
+        return anchorRecords;
     }
 }
